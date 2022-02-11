654b8a936a1593bbfe0a339fb0ba771246ba0c66
Ian D. Miller <iandouglas96@gmail.com>
2022-02-10 21:20:14 -0500

    Added cpu only switch

---
+++ src/convert_to_generic.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import torch
+from erfnet import ERFNet
+
+NUM_CLASSES = 5
+model_ = torch.nn.DataParallel(ERFNet(NUM_CLASSES)).cuda()
+
+def load_my_state_dict(model, state_dict):  #custom function to load model when not all dict elements
+    own_state = model.state_dict()
+    for name, param in state_dict.items():
+        if name not in own_state:
+             continue
+        own_state[name].copy_(param)
+    return model
+
+model_ = load_my_state_dict(model_, torch.load('../models/model_best.pth'))
+
+torch.save(model_.module.state_dict(), 'model_generic.pth')
--- src/erfnet_ros.py
+++ src/erfnet_ros.py
@@ -9,6 +9,7 @@ import cv2
 import torch
 import os
 import importlib
+import time
 
 from erfnet import ERFNet
 from transform import Relabel, ToLabel, Colorize
@@ -20,11 +21,17 @@ NUM_CHANNELS = 3
 NUM_CLASSES = 5
 
 class ERFNetWrapper:
-    def __init__(self, weights_path):
+    def __init__(self, weights_path, gpu=True):
         print ("Loading weights: " + weights_path)
+        print ("Using GPU: " + str(gpu))
 
-        self.model_ = torch.nn.DataParallel(ERFNet(NUM_CLASSES))
-        self.model_ = self.model_.cuda()
+        self.gpu_ = gpu
+        if self.gpu_:
+            self.device_ = torch.device('cuda')
+        else:
+            self.device_ = torch.device('cpu')
+
+        self.model_ = ERFNet(NUM_CLASSES).to(self.device_)
 
         def load_my_state_dict(model, state_dict):  #custom function to load model when not all dict elements
             own_state = model.state_dict()
@@ -34,7 +41,7 @@ class ERFNetWrapper:
                 own_state[name].copy_(param)
             return model
 
-        self.model_ = load_my_state_dict(self.model_, torch.load(weights_path))
+        self.model_ = load_my_state_dict(self.model_, torch.load(weights_path, map_location=self.device_))
         print ("Model and weights LOADED successfully")
         self.model_.eval()
 
@@ -44,7 +51,7 @@ class ERFNetWrapper:
         # BGR to RGB
         img_np = np.flip(img_np, axis=2)
         img_np = img_np.transpose((2, 0, 1)).astype(np.float32)/255.
-        img = torch.from_numpy(img_np[None, :]).cuda()
+        img = torch.from_numpy(img_np[None, :]).to(self.device_)
 
         with torch.no_grad():
             outputs = self.model_(img)
@@ -61,15 +68,17 @@ class ERFNetWrapper:
 class ERFNetRos:
     def __init__(self):
         self.gen_viz_ = rospy.get_param("~gen_viz", default=False)
+        gpu = rospy.get_param("~gpu", default=True)
         self.model_path_ = rospy.get_param("~model_path", default="../models/model_best.pth")
 
-        self.erfnet_ = ERFNetWrapper(self.model_path_)
+        self.erfnet_ = ERFNetWrapper(self.model_path_, gpu)
 
         self.image_sub_ = rospy.Subscriber('~image', Image, self.imageCallback, queue_size=100)
         self.label_pub_ = rospy.Publisher('~label', Image, queue_size=10)
         self.label_viz_pub_ =rospy.Publisher('~label_viz', Image, queue_size=1)
 
     def imageCallback(self, img_msg):
+        start_t = time.time()
         img = np.frombuffer(img_msg.data, dtype=np.uint8).reshape(img_msg.height, img_msg.width, -1)
         label, label_color = self.erfnet_.infer(img, self.gen_viz_)
         if label_color is not None:
@@ -91,6 +100,8 @@ class ERFNetRos:
         label_msg.step = label_msg.width
         label_msg.data = label.tobytes()
         self.label_pub_.publish(label_msg)
+        end_t = time.time()
+        rospy.loginfo(f"ErfNet total image callback time: {end_t - start_t} sec")
     
 if __name__ == '__main__':
     rospy.init_node("erfnet_ros")
